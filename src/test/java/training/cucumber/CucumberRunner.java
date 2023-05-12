343842b6d3da4a2c7982fc892c6faeba88e6b724
hang <mheng105@gmail.com>
2023-05-12 13:59:40 +0700

    update report

--- src/test/java/training/cucumber/CucumberRunner.java
+++ src/test/java/training/cucumber/CucumberRunner.java
@@ -4,8 +4,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/resources/feature"}
+        features = {"src/test/resources/feature"},
 //        glue = "classpath:src/test/java/training/cucumber/stepdefinitions"
+        plugin = {"pretty",
+                "html:target/report/cucumber.html",
+                "json:target/report/cucumber.json"
+        }
+
 )
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
